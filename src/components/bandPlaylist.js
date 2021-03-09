d333bc5f2238cae3f166aa0ca163ae754eb04d96
bgdtomas <mtmbogado21@gmail.com>
2021-03-09 15:36:13 -0300

    responsiveness to most components

--- src/components/bandPlaylist.js
+++ src/components/bandPlaylist.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import albumCover from "../images/SBOvsTheWORLD.jpg";
 
 class BandPlaylist extends React.Component {
   render() {
     return (
+      <div>
+        <h2>
+          AVAILABLE FOR STREAM NOW
+        </h2>
+        <h1 className="biggerText">
+          "SCOTT PILGRIM VS THE WORLD"
+        </h1>
         <div className="playlistContainer">
-          <div className="albumImage">
+          <div>
+            <img src={albumCover} className="albumImage"/>
           </div>
           <div className="songs">
             <div>WE ARE SEX BOB OMB</div>
@@ -14,9 +22,9 @@ class BandPlaylist extends React.Component {
             <div>SUMMERTIME</div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-
+}
 
 export default BandPlaylist;
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,22 +3,23 @@ import Banner from "../components/banner";
 import BandInfo from "../components/bandInfo";
 import MembersInfo from "../components/membersInfo";
 import BandPlaylist from "../components/bandPlaylist";
+import BandBackground from "../images/BANDBACKGROUND.png";
 
-class LandingPage extends React.Component{
-
-    render(){
-        return(
-            <div>
-            <Banner/>
-            <BandInfo/>
-            <MembersInfo/>
-            <div className="bandRocking"></div>
-            <BandPlaylist/>
-            <div>hello world</div>
-            </div>
-        );
-    }
-
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <BandInfo />
+        <MembersInfo />
+        <div className="bandRocking">
+          <img src={BandBackground} />
+        </div>
+        <BandPlaylist />
+        <div>hello world</div>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
